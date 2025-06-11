64dd36db23cb0c784a3e81256cbc4d585f8bef99
j.l.76m.b@gmail.comgit config --global user.email j.l.76m.b@gmail.com <j.l.76.m.b@gmail.com>
2025-06-11 12:38:25 -0600

    agregue un combobox

--- Bamcodex0.2/Servicios2.Designer.vb
+++ Bamcodex0.2/Servicios2.Designer.vb
@@ -27,10 +27,10 @@ Partial Class Servicios2
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
         '
         'Label1
@@ -39,9 +39,10 @@ Partial Class Servicios2
         Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Britannic Bold", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(452, 54)
+        Me.Label1.Location = New System.Drawing.Point(339, 44)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(204, 44)
+        Me.Label1.Size = New System.Drawing.Size(165, 36)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "SERVICIOS"
         '
@@ -51,9 +52,10 @@ Partial Class Servicios2
         Me.Label2.BackColor = System.Drawing.Color.Transparent
         Me.Label2.Font = New System.Drawing.Font("Britannic Bold", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.Color.White
-        Me.Label2.Location = New System.Drawing.Point(434, 124)
+        Me.Label2.Location = New System.Drawing.Point(326, 101)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(255, 31)
+        Me.Label2.Size = New System.Drawing.Size(201, 25)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Ingrese el convenio"
         '
@@ -63,9 +65,10 @@ Partial Class Servicios2
         Me.Label3.BackColor = System.Drawing.Color.Transparent
         Me.Label3.Font = New System.Drawing.Font("Britannic Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.ForeColor = System.Drawing.Color.White
-        Me.Label3.Location = New System.Drawing.Point(345, 189)
+        Me.Label3.Location = New System.Drawing.Point(259, 154)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(196, 22)
+        Me.Label3.Size = New System.Drawing.Size(158, 17)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Numero del convenio"
         '
@@ -75,9 +78,10 @@ Partial Class Servicios2
         Me.Label4.BackColor = System.Drawing.Color.Transparent
         Me.Label4.Font = New System.Drawing.Font("Britannic Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label4.ForeColor = System.Drawing.Color.White
-        Me.Label4.Location = New System.Drawing.Point(345, 290)
+        Me.Label4.Location = New System.Drawing.Point(259, 236)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(201, 22)
+        Me.Label4.Size = New System.Drawing.Size(158, 17)
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "Captura la referencia"
         '
@@ -87,26 +91,12 @@ Partial Class Servicios2
         Me.Label5.BackColor = System.Drawing.Color.Transparent
         Me.Label5.Font = New System.Drawing.Font("Britannic Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label5.ForeColor = System.Drawing.Color.White
-        Me.Label5.Location = New System.Drawing.Point(345, 394)
+        Me.Label5.Location = New System.Drawing.Point(259, 320)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(167, 22)
+        Me.Label5.Size = New System.Drawing.Size(133, 17)
         Me.Label5.TabIndex = 4
         Me.Label5.Text = "Importe a pagar :"
-        '
-        'Button1
-        '
-        Me.Button1.BackColor = System.Drawing.Color.Transparent
-        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
-        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
-        Me.Button1.Font = New System.Drawing.Font("Britannic Bold", 13.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.ForeColor = System.Drawing.Color.White
-        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button1.Location = New System.Drawing.Point(340, 223)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(410, 51)
-        Me.Button1.TabIndex = 33
-        Me.Button1.Text = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-        Me.Button1.UseVisualStyleBackColor = False
         '
         'Button2
         '
@@ -116,9 +106,10 @@ Partial Class Servicios2
         Me.Button2.Font = New System.Drawing.Font("Britannic Bold", 13.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button2.ForeColor = System.Drawing.Color.White
         Me.Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button2.Location = New System.Drawing.Point(340, 324)
+        Me.Button2.Location = New System.Drawing.Point(255, 263)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(410, 53)
+        Me.Button2.Size = New System.Drawing.Size(308, 43)
         Me.Button2.TabIndex = 34
         Me.Button2.Text = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
         Me.Button2.UseVisualStyleBackColor = False
@@ -131,9 +122,10 @@ Partial Class Servicios2
         Me.Button3.Font = New System.Drawing.Font("Britannic Bold", 13.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button3.ForeColor = System.Drawing.Color.White
         Me.Button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button3.Location = New System.Drawing.Point(349, 432)
+        Me.Button3.Location = New System.Drawing.Point(262, 351)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(401, 53)
+        Me.Button3.Size = New System.Drawing.Size(301, 43)
         Me.Button3.TabIndex = 35
         Me.Button3.Text = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
         Me.Button3.UseVisualStyleBackColor = False
@@ -146,30 +138,40 @@ Partial Class Servicios2
         Me.Button4.Font = New System.Drawing.Font("Britannic Bold", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button4.ForeColor = System.Drawing.Color.White
         Me.Button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button4.Location = New System.Drawing.Point(808, 479)
+        Me.Button4.Location = New System.Drawing.Point(606, 389)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(332, 102)
+        Me.Button4.Size = New System.Drawing.Size(249, 83)
         Me.Button4.TabIndex = 36
         Me.Button4.Text = "Realizar pago"
         Me.Button4.UseVisualStyleBackColor = False
         '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(255, 192)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(308, 21)
+        Me.ComboBox1.TabIndex = 37
+        '
         'Servicios2
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.Bamcodex0._2.My.Resources.Resources.Captura_de_pantalla_2025_06_11_024634
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(1141, 583)
+        Me.ClientSize = New System.Drawing.Size(856, 474)
+        Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.DoubleBuffered = True
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Servicios2"
         Me.Text = "Servicios2"
         Me.ResumeLayout(False)
@@ -182,8 +184,8 @@ Partial Class Servicios2
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
-    Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
+    Friend WithEvents ComboBox1 As ComboBox
 End Class
--- Bamcodex0.2/Servicios2.vb
+++ Bamcodex0.2/Servicios2.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Servicios2
+    Private Sub Servicios2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
 End Class
