c2f96170422f40579ae21e7e89cb2625e776e935
j.l.76m.b@gmail.comgit config --global user.email j.l.76m.b@gmail.com <j.l.76.m.b@gmail.com>
2025-06-11 12:23:57 -0600

    cambio 1

--- Bamcodex0.2/Confirmar.Designer.vb
+++ Bamcodex0.2/Confirmar.Designer.vb
@@ -26,7 +26,6 @@ Partial Class Confirmar
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
@@ -38,9 +37,10 @@ Partial Class Confirmar
         Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Britannic Bold", 25.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(16, 33)
+        Me.Label1.Location = New System.Drawing.Point(12, 27)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(330, 48)
+        Me.Label1.Size = New System.Drawing.Size(271, 38)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Confirmar datos"
         '
@@ -50,9 +50,10 @@ Partial Class Confirmar
         Me.Label2.BackColor = System.Drawing.Color.Transparent
         Me.Label2.Font = New System.Drawing.Font("Britannic Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.Color.White
-        Me.Label2.Location = New System.Drawing.Point(21, 140)
+        Me.Label2.Location = New System.Drawing.Point(16, 114)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(244, 26)
+        Me.Label2.Size = New System.Drawing.Size(202, 21)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Nro de cuenta o tarjeta"
         '
@@ -62,9 +63,10 @@ Partial Class Confirmar
         Me.Label4.BackColor = System.Drawing.Color.Transparent
         Me.Label4.Font = New System.Drawing.Font("Britannic Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label4.ForeColor = System.Drawing.Color.White
-        Me.Label4.Location = New System.Drawing.Point(18, 293)
+        Me.Label4.Location = New System.Drawing.Point(14, 238)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(209, 26)
+        Me.Label4.Size = New System.Drawing.Size(174, 21)
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "Importe a depositar"
         '
@@ -74,26 +76,12 @@ Partial Class Confirmar
         Me.Label6.BackColor = System.Drawing.Color.Transparent
         Me.Label6.Font = New System.Drawing.Font("Britannic Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label6.ForeColor = System.Drawing.Color.White
-        Me.Label6.Location = New System.Drawing.Point(21, 427)
+        Me.Label6.Location = New System.Drawing.Point(16, 347)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(134, 26)
+        Me.Label6.Size = New System.Drawing.Size(111, 21)
         Me.Label6.TabIndex = 5
         Me.Label6.Text = "Destinatario"
-        '
-        'Button1
-        '
-        Me.Button1.BackColor = System.Drawing.Color.Transparent
-        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
-        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
-        Me.Button1.Font = New System.Drawing.Font("Britannic Bold", 13.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.ForeColor = System.Drawing.Color.White
-        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button1.Location = New System.Drawing.Point(14, 183)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(524, 59)
-        Me.Button1.TabIndex = 31
-        Me.Button1.Text = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-        Me.Button1.UseVisualStyleBackColor = False
         '
         'Button2
         '
@@ -103,9 +91,10 @@ Partial Class Confirmar
         Me.Button2.Font = New System.Drawing.Font("Britannic Bold", 13.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button2.ForeColor = System.Drawing.Color.White
         Me.Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button2.Location = New System.Drawing.Point(14, 333)
+        Me.Button2.Location = New System.Drawing.Point(10, 271)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(348, 45)
+        Me.Button2.Size = New System.Drawing.Size(261, 37)
         Me.Button2.TabIndex = 32
         Me.Button2.Text = "$ 0.0"
         Me.Button2.UseVisualStyleBackColor = False
@@ -118,9 +107,10 @@ Partial Class Confirmar
         Me.Button3.Font = New System.Drawing.Font("Britannic Bold", 13.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button3.ForeColor = System.Drawing.Color.White
         Me.Button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button3.Location = New System.Drawing.Point(14, 465)
+        Me.Button3.Location = New System.Drawing.Point(10, 378)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(348, 45)
+        Me.Button3.Size = New System.Drawing.Size(261, 37)
         Me.Button3.TabIndex = 33
         Me.Button3.Text = "Lorem  Ipsum"
         Me.Button3.UseVisualStyleBackColor = False
@@ -133,29 +123,30 @@ Partial Class Confirmar
         Me.Button4.Font = New System.Drawing.Font("Britannic Bold", 22.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button4.ForeColor = System.Drawing.Color.White
         Me.Button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button4.Location = New System.Drawing.Point(845, 512)
+        Me.Button4.Location = New System.Drawing.Point(634, 416)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(264, 89)
+        Me.Button4.Size = New System.Drawing.Size(198, 72)
         Me.Button4.TabIndex = 34
         Me.Button4.Text = "Confirmar"
         Me.Button4.UseVisualStyleBackColor = False
         '
         'Confirmar
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.Bamcodex0._2.My.Resources.Resources.Captura_de_pantalla_2025_06_11_022550
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(1109, 606)
+        Me.ClientSize = New System.Drawing.Size(832, 492)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.DoubleBuffered = True
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Confirmar"
         Me.Text = "Confirmar"
         Me.ResumeLayout(False)
@@ -167,7 +158,6 @@ Partial Class Confirmar
     Friend WithEvents Label2 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label6 As Label
-    Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
