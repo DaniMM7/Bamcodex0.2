264a5d61d8005f94cd71eea20500b542a8a7e277
Francisco <Francisco@DESKTOP-9S367KS>
2025-06-11 22:49:27 -0600

    Deposito Actualizado

--- Bamcodex0.2/Cargar.vb
+++ Bamcodex0.2/Cargar.vb
@@ -5,7 +5,7 @@ Module Cargar
     Public base As String
     Public Sub conectar()
         conexion_general = New SqlConnection
-        conexion_general.ConnectionString = ("server=BROUNAS\JUANB; database=Bancodexxdb; integrated security=true")
+        conexion_general.ConnectionString = ("server=DESKTOP-9S367KS\INSTANCIA; database=Bancodexxdb; integrated security=true")
         conexion_general.Open()
     End Sub
 End Module
